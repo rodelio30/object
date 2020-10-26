8a961098f0d66c0a49a454f5e35099fd9d59ebdb
rodelio30 <rodeliobdomingojr@gmail.com>
2020-10-26 16:15:07 +0800

    finalize todo

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,5 +1,6 @@
 class TodoItem < ApplicationRecord
   belongs_to :todo_list
+  comp = true
   def completed?
     !completed_at.blank?
   end
--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,6 +1,6 @@
 class TodoList < ApplicationRecord
-  has_many :todo_items
-  
+  has_many :todo_items, dependent: :destroy
+
   validates :title, presence: true
   validates :description, presence: true
 end
